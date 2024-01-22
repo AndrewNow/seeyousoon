555cf2d18bfe18abc4b4e9ebcc2da0f764e24d28
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-01-22 16:41:56 -0500

    pushing

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import { sanityIntegration } from "@sanity/astro";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
