5194feaa216d445aad0a90dd5a12bd61e759973d
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-01-22 16:33:57 -0500

    pushing

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from "astro/config";
 import { sanityIntegration } from "@sanity/astro";
-// import vercel from '@astrojs/vercel/serverless';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  // adapter: vercel({
-  //   edgeMiddleware: true,
-  //   maxDuration: 8
-  // }),
   integrations: [
     sanityIntegration({
       projectId: "gt0shs9f",
