4ee3bd3a5dde74b2178a69e32287bac26460db6d
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-01-22 15:41:17 -0500

    adding ssr

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { sanityIntegration } from "@sanity/astro";
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     sanityIntegration({
       projectId: 'gt0shs9f',
