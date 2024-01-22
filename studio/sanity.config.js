65e56ce0f8b9801e66a444576c1d904c484150a2
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-01-22 12:52:21 -0500

    initial commit

---
+++ studio/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sys-demo',
+
+  projectId: 'gt0shs9f',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
