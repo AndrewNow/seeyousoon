04d00391865db063dc01afe286f03f4892c5fa27
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-02-20 12:33:00 -0500

    adding animation for test2 page

---
+++ web/src/components/utils/marquee.js
@@ -0,0 +1,50 @@
+ import marquee from "vanilla-marquee";
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const titleMarquees = document.querySelectorAll("#title-marquee");
+
+    titleMarquees.forEach((el) => {
+      new marquee(el, {
+        duplicated: true,
+        gap: 30,
+        speed: 100,
+        // pauseOnHover: true,
+        startVisible: true,
+        recalcResize: true,
+      });
+    });
+
+    const pastEventsMarquees = document.querySelectorAll("#pastEvents-marquee");
+    // Calculate marquee speed based on the number of images
+    const calculateMarqueeSpeed = (imageCount) => {
+      // Adjust these values as needed to control the speed based on the image count
+      const minSpeed = 10; // Lower bound speed
+      const maxSpeed = 100; // Upper bound speed
+
+      // Calculate the speed based on the image count
+      const calculatedSpeed =
+        minSpeed + (maxSpeed - minSpeed) * (imageCount / 10);
+
+      return calculatedSpeed;
+    };
+
+    console.log("pastEventsMarquees", pastEventsMarquees);
+
+    pastEventsMarquees.forEach((el) => {
+      const marqueeSpeed = calculateMarqueeSpeed(
+        el.getAttribute("data-image-count")
+      );
+      // const imageCount = el.getAttribute("data-image-count");
+      console.log("imagecount", marqueeSpeed);
+      const speed = marqueeSpeed ? marqueeSpeed : 50;
+      new marquee(el, {
+        duplicated: true,
+        gap: 0,
+        speed: speed,
+        // pauseOnHover: true,
+        startVisible: true,
+        // recalcResize: true,
+        recalcResize: true,
+      });
+    });
+  });
