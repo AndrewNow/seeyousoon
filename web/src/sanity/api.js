cf8b5fdf7e5bea5a4be459dec005e63260f62029
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-02-26 15:39:15 -0500

    adding loading state for images on desktop, adding toast WIP

--- web/src/sanity/api.js
+++ web/src/sanity/api.js
@@ -16,3 +16,36 @@ export async function getPastEvents() {
   }`)
   return pastEvents
 }
+
+export async function getUpcomingEvents() {
+  const pastEvents = await sanityClient.fetch(
+    `*[_type == "upcomingEvents"]{
+    _id,
+    title,
+    isResidency,
+    eventLink,
+    dateStart,
+    dateEnd,
+    location,
+    locationLink
+  }`)
+  return pastEvents
+}
+
+
+export async function getUpcomingEvent() {
+  const upcomingEvent = await sanityClient.fetch(
+    `*[_type == "upcomingEvents" && dateStart >= now()] | order(dateStart asc) [0] {
+      _id,
+      title,
+      isResidency,
+      eventLink,
+      dateStart,
+      dateEnd,
+      location,
+      locationLink
+    }`
+  );
+
+  return upcomingEvent;
+}
